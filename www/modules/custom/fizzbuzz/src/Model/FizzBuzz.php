96e5fc5049272242c800d96425bfa69c8f3296e5
Interview <interview@Igors-MBP.lan>
2016-11-07 16:09:53 +0000

    FizzBuzz Corrected

--- www/modules/custom/fizzbuzz/src/Model/FizzBuzz.php
+++ www/modules/custom/fizzbuzz/src/Model/FizzBuzz.php
@@ -44,7 +44,17 @@ class FizzBuzz {
    */
   public static function generate() {
     return array_map(function($value) {
-      return self::parseFizzBuzzNumber($value);
+      $multithree = $value % 3;
+      $multifive = $value % 5;
+      if($multithree == 0 && $multifive == 0){
+        return "FizzBuzz";
+      } elseif ($multithree == 0){
+        return "Fizz";
+      } elseif ($multifive == 0){
+        return "Buzz";
+      } else {
+        return self::parseFizzBuzzNumber($value);
+      }
     }, range(self::START_NUMBER, self::FINAL_NUMBER));
   }
 
